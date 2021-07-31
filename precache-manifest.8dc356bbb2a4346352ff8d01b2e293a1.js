54d70ce4f13cc89962d71c1474574f6404fd61e3
VolodymyrKlepykov <volodymyr.klepykov@gmail.com>
2021-07-31 19:26:06 +0300

    Updates

---
+++ precache-manifest.8dc356bbb2a4346352ff8d01b2e293a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff374d1fc47a02c83139c4aa06582b69",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4febbb437ef1e9644263",
+    "url": "./static/css/2.a1df12c0.chunk.css"
+  },
+  {
+    "revision": "7fc6dfb0e535a55df844",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "4febbb437ef1e9644263",
+    "url": "./static/js/2.c76e139b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c76e139b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fc6dfb0e535a55df844",
+    "url": "./static/js/main.1e5dd4e1.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
